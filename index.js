ece310576bd93f02f9fed972dfec69bf6e63b6a2
Carlos Matos <carlos.matos@uhuu.com>
2023-07-25 12:10:51 -0300

    Cleaning the comments

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // alert('It works!');
 
-// let favoriteFood = 'Pizza';
-// document.getElementById('text').innerHTML = favoriteFood;
+let favoriteFood = 'Pizza';
+document.getElementById('text').innerHTML = favoriteFood;
 
 /**
  *  OBJECT
  */
 
-/*
+
 const book = {
     bookTitle:  'The Hobbit',
     bookAuthor: 'Tolkien',
@@ -21,57 +21,58 @@ const book = {
         console.log('Printing...')
     }
 }
-*/
+
 
 /**
  * FACTORY
  */
 
-//function CreateBook(title, author, pages) {
+function CreateBook(title, author, pages) {
 
-//     const book = {
-//         bookTitle:  title,
-//         bookAuthor: author,
-//         bookPages:  pages,
-//         printBook: function() {
-//             console.log('Printing...')
-//         }
-//     }
+    const book = {
+        bookTitle:  title,
+        bookAuthor: author,
+        bookPages:  pages,
+        printBook: function() {
+            console.log('Printing...')
+        }
+    }
 
-//     return book;
-// }
+    return book;
+}
 
-// const book1 = createBook('The Hobbit', 'Tolkien', 310);
-// console.log(book1);
+const book2 = createBook('The Hobbit', 'Tolkien', 310);
+console.log(book2);
 
 /***
  * CONSTRUCTOR - uses 
+ * 
  */
-//     this.bookTitle = title;
-//     this.bookAuthor = author;
-//     this.bookPages = pages
-// }
+    this.bookTitle = title;
+    this.bookAuthor = author;
+    this.bookPages = pages
 
-// const book1 = new CreateBook('The Hobbit', 'Tolkien', 310);
-// console.log(book1);
+
+const book1 = new CreateBook('The Hobbit', 'Tolkien', 310);
+console.log(book1);
 
 /**
  * BUILT-IN OBJECTS
  */
 
-// console.log(Math.max(1, 5, 0, 30));
-// console.log(Math.min(1, 3, 5, 2));
-// console.log(Math.PI);
+console.log(Math.max(1, 5, 0, 30));
+console.log(Math.min(1, 3, 5, 2));
+console.log(Math.PI);
 
 // Built-in String
 
-// let message = 'Hello World!';
-// console.log(message.startsWith('hello')); // false
-// console.log(message.startsWith('Hello')); // true
-// console.log(message.length);
-// console.log(message.includes('Hello')); // true
-// console.log(message.endsWith('e')); // false
-// console.log(message.endsWith('!')); // true
+let message = 'Hello World!';
+console.log(message.startsWith('hello')); // false
+console.log(message.startsWith('Hello')); // true
+console.log(message.length);
+console.log(message.includes('Hello')); // true
+console.log(message.endsWith('e')); // false
+console.log(message.endsWith('!')); // true
 
 /**
  * TEMPLATE LITERALS
