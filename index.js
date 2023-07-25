6b8fdb8b7a0aaf834ab0f6c3df60db60adcbf86a
Carlos Matos <carlos.matos@uhuu.com>
2023-07-25 11:56:04 -0300

    Built-in String

--- index.js
+++ index.js
@@ -59,6 +59,16 @@ const book = {
  * BUILT-IN OBJECTS
  */
 
-console.log(Math.max(1, 5, 0, 30));
-console.log(Math.min(1, 3, 5, 2));
-console.log(Math.PI);
+// console.log(Math.max(1, 5, 0, 30));
+// console.log(Math.min(1, 3, 5, 2));
+// console.log(Math.PI);
+
+// Built-in String
+
+let message = 'Hello World!';
+console.log(message.startsWith('hello')); // false
+console.log(message.startsWith('Hello')); // true
+console.log(message.length);
+console.log(message.includes('Hello')); // true
+console.log(message.endsWith('e')); // false
+console.log(message.endsWith('!')); // true
