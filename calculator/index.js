f0c13c4160e0391dc6a65c9adf0c0ea9fda7e24b
Carlos Matos <carlos.matos@uhuu.com>
2023-08-08 16:02:52 -0300

    Function declaration and Function expression

--- calculator/index.js
+++ calculator/index.js
@@ -1,22 +1,15 @@
-function outcome() {
-    let num1 = Number(document.getElementById('num-one').value);
-    let num2 = Number(document.getElementById('num-two').value);
-    let total = 0;
+// Function declaration
 
-    if(document.getElementById('box1').checked) {
-        total = num1 + num2;
-    }
-    else if(document.getElementById('box2').checked) {
-        total = num1 - num2;
-    }
-    else if(document.getElementById('box3').checked) {
-        total = num1 * num2;
-    }
-    else {
-        total = num1 / num2;
-    }
-
-    document.getElementById('resultArea').innerHTML = 'Result: ' + String(total);
+function movie() {
+    console.log('The Matrix');
 }
 
-outcome
+movie();
+
+// Function expression
+
+const car = function() {
+    console.log('BMW');
+}
+
+car();
